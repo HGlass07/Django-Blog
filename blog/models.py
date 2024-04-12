050f6007981389e5cc7f8070aa40c71fce592d56
Henry <hglasspool@googlemail.com>
2024-04-12 10:00:22 +0000

    added meta and _str_ class to comment model

--- blog/models.py
+++ blog/models.py
@@ -21,16 +21,26 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"The title of this post is {self.title}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author"
     )
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
