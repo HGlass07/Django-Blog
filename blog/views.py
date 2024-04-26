4fdab0f1ea44e02a703b1d2727d167106e6b0c8e
Henry <hglasspool@googlemail.com>
2024-04-26 11:34:12 +0000

    placed post-detail view in correct views section

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,12 @@ from django.views import generic
 from .models import Post
 
 
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.order_by("-created_on")
+    template_name = "blog/index.html"
+    paginate_by = 6
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -16,18 +22,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "templates/post_detail.html",
-        {"post": post},
-    )
-
-# Create your views here.
-class PostList(generic.ListView):
-    queryset = Post.objects.order_by("-created_on")
-    template_name = "blog/index.html"
-    paginate_by = 6
+    return render(request, "blog/post_detail.html", {"post": post},)
