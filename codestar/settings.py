905de80a22f5b7a65ee6cff92e23c59602706b05
Henry <hglasspool@googlemail.com>
2024-04-26 15:56:55 +0000

    test

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['8000-hglass07-djangoblog-mndrb68eo6e.ws-eu110.gitpod.io'
+ALLOWED_HOSTS = ['8000-hglass07-djangoblog-mndrb68eo6e.ws-us110.gitpod.io'
 , '.herokuapp.com']
 
 
