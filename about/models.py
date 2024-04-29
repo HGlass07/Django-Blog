1b8516cc37704de9db8376c83a1c55a9a64cf86c
Henry <hglasspool@googlemail.com>
2024-04-29 09:36:22 +0000

    completed url, views, model, updated html for about app

--- about/models.py
+++ about/models.py
@@ -5,10 +5,7 @@ from django.contrib.auth.models import User
 class Admin(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-created_on"]
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return self.title
